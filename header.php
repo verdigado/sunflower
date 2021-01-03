014d1cf515af0ce6279b04e222e179d42e269154
Tom Rose <mail@tom-rose.de>
2021-01-03 17:41:38 +0100

    add logo file

--- header.php
+++ header.php
@@ -54,7 +54,9 @@
 
 		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 			<div class="container">
-				<a class="navbar-brand" href="<?php echo get_home_url(); ?>">Home</a>
+				<a class="navbar-brand" href="<?php echo get_home_url(); ?>">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/sunflower.svg" class="">
+				</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 				</button>
