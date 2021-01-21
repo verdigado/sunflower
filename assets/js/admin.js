ca8056c9d075352ca036bbc5d5f49adb262a957e
Tom Rose <mail@tom-rose.de>
2021-01-21 20:37:12 +0100

    lint

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,10 +1,9 @@
-jQuery(document).on( 'click', '.sunflower-plugins .notice-dismiss', function() {
-
-    jQuery.ajax({
-        url: ajaxurl,
-        data: {
-            action: 'sunflower_plugins_dismiss'
-        }
-    })
-
-})
+/* eslint-disable no-undef */
+jQuery( document ).on( 'click', '.sunflower-plugins .notice-dismiss', function() {
+	jQuery.ajax( {
+		url: ajaxurl,
+		data: {
+			action: 'sunflower_plugins_dismiss',
+		},
+	} );
+} );
