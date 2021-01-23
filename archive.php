603ecf7468754f6176a7478628aedd851d938655
Tom Rose <mail@tom-rose.de>
2021-01-23 14:58:59 +0100

    wip: masonry

--- archive.php
+++ archive.php
@@ -22,11 +22,11 @@ get_header();
 							?>
 						</header><!-- .page-header -->
 
+						<div class="archive-loop">
 						<?php
 						/* Start the Loop */
 						
-						$columns = ['', ''];
-						$i = 0;
+
 						while ( have_posts() ) :
 							the_post();
 
@@ -35,25 +35,12 @@ get_header();
 							* If you want to override this in a child theme, then include a file
 							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
 							*/
-							ob_start();
-								get_template_part( 'template-parts/content', 'archive' );
-								$columns[$i] .= ob_get_clean();
-					
-
-							$i = ($i + 1 ) % 2;
-							
+							get_template_part( 'template-parts/content', 'archive' );
+			
 						endwhile;
-					?>
 
-						<div class="archive-loop row">
-							<div class="col-12 col-md-6">
-								<?php echo $columns[0]; ?>
-							</div>
-							<div class="col-12 col-md-6">
-								<?php echo $columns[1]; ?>
-							</div>
-						</div>
-					<?php
+						echo '</div>'; // end .archive-loop
+						
 						the_posts_navigation();
 
 					else :
