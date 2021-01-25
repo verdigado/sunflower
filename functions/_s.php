a199623384677d690a463af3cd107405aabcd147
Tom Rose <mail@tom-rose.de>
2021-01-25 15:24:54 +0100

    add jquery slim via npm

--- functions/_s.php
+++ functions/_s.php
@@ -170,7 +170,7 @@ function sunflower_scripts() {
 	
 	wp_enqueue_script(
         'jquery-slim',
-        get_template_directory_uri() . '/assets/vndr/jquery-3.2.1.slim.min.js',
+        get_template_directory_uri() . '/assets/vndr/jquery-slim/dist/jquery.slim.min.js',
         null,
         '3.2.1', 
         true
