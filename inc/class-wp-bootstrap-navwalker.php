45a963e71a8d38908d1179e3f4c47554afeeed46
Tom Rose <mail@tom-rose.de>
2021-02-09 23:15:29 +0100

    fix toggler for bootstrap5

--- inc/class-wp-bootstrap-navwalker.php
+++ inc/class-wp-bootstrap-navwalker.php
@@ -181,7 +181,7 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) {
 			// If item has_children add atts to <a>.
 			if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && $args->depth > 1 ) {
 				$atts['href']          = '#';
-				$atts['data-toggle']   = 'dropdown';
+				$atts['data-bs-toggle']   = 'dropdown';
 				$atts['aria-haspopup'] = 'true';
 				$atts['aria-expanded'] = 'false';
 				$atts['class']         = 'dropdown-toggle nav-link';
