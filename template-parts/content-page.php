1b84c2483250f8815e3db9b1446737466075fe05
Tom Rose <mail@tom-rose.de>
2021-02-09 23:19:18 +0100

    delete edit post link

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -32,27 +32,4 @@ $show_title = @get_post_meta( $post->ID, '_sunflower_show_title')[0] ? true : fa
 		);
 		?>
 	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'sunflower' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
