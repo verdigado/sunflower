cb6db0bdac94872be11b47e8f6fae1152a2f00da
Tom Rose <mail@tom-rose.de>
2021-02-13 23:22:36 +0100

    Revert "remove font-awesome in backend"
    This reverts commit 75b9a26210ec0707e41a36f799bf428f94af34cc.

--- functions/admin.php
+++ functions/admin.php
@@ -73,6 +73,7 @@ add_action( 'wp_ajax_sunflowerCreateHomepage', 'sunflower_createHomepage' );
 
 
 function sunflower_admin_style() {
+    wp_enqueue_style('sunflower-admin-fontawesome', get_template_directory_uri().'/assets/css/admin-fontawesome.css');
     wp_enqueue_style('sunflower-admin-styles', get_template_directory_uri().'/assets/css/admin.css');
  }
  
