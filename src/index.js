31d9d1a42cffd54e513b15caac253e78ae728f1b
Tom Rose <mail@tom-rose.de>
2021-02-13 23:39:27 +0100

    use meta-data-line again

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import * as nextEvents from './next-events.js';
 import * as latestPosts from './latest-posts.js';
-//import * as metaDataLine from './meta-data-line.js';
+import * as metaDataLine from './meta-data-line.js';
 
