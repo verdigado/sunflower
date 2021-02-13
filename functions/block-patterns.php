2cea8726270b68559a1d1e73d3cc8023c9331280
Tom Rose <mail@tom-rose.de>
2021-02-13 23:41:39 +0100

    add automatic blockpatterns

--- functions/block-patterns.php
+++ functions/block-patterns.php
@@ -1,18 +1,28 @@
 <?php
 
 if( function_exists( 'register_block_pattern_category') ){
-    register_block_pattern_category(
-        'sunflower',
-        array( 'label' => esc_html__( 'Sunflower', 'sunflower' ) )
-    );
+    $dirs = glob(get_stylesheet_directory() . '/functions/block-patterns/*', GLOB_ONLYDIR);
 
-    register_block_pattern(
-        'sunflower/person',
-        array(
-            'title'       => __( 'Person', 'sunflower' ),
-            'description' => _x( 'Describes a person.', 'Block pattern description', 'function' ),
-            'categories'  => ['sunflower'],
-            'content'     => file_get_contents( get_stylesheet_directory() . '/functions/block-patterns/person.html'),
-        )
-    );
+    foreach($dirs AS $dir){
+        $basenameDir = basename($dir);
+
+        register_block_pattern_category(
+            'sunflower-' . $basenameDir,
+            array( 'label' => esc_html__( 'Sunflower', 'sunflower' ) . '-' . ucfirst($basenameDir))
+        );
+   
+        $files = glob( $dir . '/*.html' );
+        foreach($files AS $file){
+            $basenameFile = basename($file, '.html');
+
+            register_block_pattern(
+                'sunflower/' . $basenameFile,
+                array(
+                    'title'       => ucfirst($basenameFile),
+                    'categories'  => ['sunflower-' . $basenameDir],
+                    'content'     => file_get_contents( $file ),
+                )
+            );
+        }
+    }
 }
