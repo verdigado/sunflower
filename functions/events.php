9e693b156876123f4db32b81d6c83377446cbf26
Tom Rose <mail@tom-rose.de>
2021-02-19 22:40:25 +0100

    add translation

--- functions/events.php
+++ functions/events.php
@@ -1,24 +1,22 @@
 <?php
 
 $sunflower_event_fields = [
-    '_sunflower_event_from'            => [ 'Startdate', 'datetimepicker' ],
-    '_sunflower_event_until'           => [ 'Enddate', 'datetimepicker' ],
-    '_sunflower_event_whole_day'       => [ 'Whole day', null, 'checkbox' ],
-    '_sunflower_event_location_name'   => [ 'Location name' ],
-    '_sunflower_event_location_street' => [ 'Street'],
-    '_sunflower_event_location_city'   => [ 'City' ],
-    '_sunflower_event_webinar'   => [ 'Webinar' ],
-    '_sunflower_event_organizer'   => [ 'Organizer' ],
-    '_sunflower_event_organizer_url'   => [ 'Organizer URL' ],
-
-
+    '_sunflower_event_from'            => [ __('Startdate', 'sunflower'), 'datetimepicker' ],
+    '_sunflower_event_until'           => [ __('Enddate', 'sunflower'), 'datetimepicker' ],
+    '_sunflower_event_whole_day'       => [ __('Whole day', 'sunflower'), null, 'checkbox' ],
+    '_sunflower_event_location_name'   => [ __('Location name', 'sunflower') ],
+    '_sunflower_event_location_street' => [ __('Street', 'sunflower')],
+    '_sunflower_event_location_city'   => [ __('City', 'sunflower') ],
+    '_sunflower_event_webinar'         => [ __('Webinar', 'sunflower') ],
+    '_sunflower_event_organizer'       => [ __('Organizer', 'sunflower') ],
+    '_sunflower_event_organizer_url'   => [ __('Organizer URL', 'sunflower') ],
     '_sunflower_event_lat'    => [ 'Latitude', null, 'hidden'],
     '_sunflower_event_lon'    => [ 'Longitude', null, 'hidden'],
     '_sunflower_event_zoom'   => [ 'Zoom', null, 'hidden'],
 ];
 
 function sunflower_create_event_post_type() {
- 
+
     register_post_type( 'sunflower_event',
     // CPT Options
         array(
@@ -164,7 +162,7 @@ function sunflower_event_meta_box(){
 }
 
 function sunflower_event_field( $id, $config, $value ){
-    $label = __($config[0], 'sunflower');
+    $label = $config[0];
     $class = @$config[1] ?: '';
     $type = @$config[2] ?: false;
 
