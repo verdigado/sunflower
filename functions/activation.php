77e443ee588127c6e0cfcc00a57c399f45399256
Tom Rose <mail@tom-rose.de>
2021-02-19 08:04:55 +0100

    use theme_mods_urwahl300 for widget import

--- functions/activation.php
+++ functions/activation.php
@@ -7,10 +7,14 @@ function sunflower_activate_theme( $old_theme_name, $old_theme = false ){
 
 }
 
-function sunflower_import_widgets(){    
+function sunflower_import_widgets(){
+    // check for theme_mods_urwahl3000
+    $options = get_option('theme_mods_urwahl3000');  
+    $sidebar_widgets = array_merge($options['sidebar_widgets']['infospalte'], $options['sidebar_widgets']['fussleist'] );
+    
     $option = get_option('sidebars_widgets');                  
     if( empty($option['sidebar-1'] ) ){
-        $option['sidebar-1'] = $option['wp_inactive_widgets'];
+        $option['sidebar-1'] = $sidebar_widgets; //$option['wp_inactive_widgets'];
         update_option('sidebars_widgets', $option);
 
     }
