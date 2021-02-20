59fe875f14d1c543eee286cf6c13cfbb37e597f3
Tom Rose <mail@tom-rose.de>
2021-02-20 23:29:52 +0100

    add login logo

--- functions/admin.php
+++ functions/admin.php
@@ -98,3 +98,14 @@ function sunfloer_setup_help_tab() {
     }
     }
     add_action( 'admin_head', 'sunfloer_setup_help_tab' );
+
+    function sunflower_login_logo() { ?>
+        <style type="text/css">
+            #login h1 a, .login h1 a {
+                background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sunflower.svg);
+                padding-bottom: 30px;
+                filter: drop-shadow(2px 4px 14px gray);
+            }
+        </style>
+    <?php }
+    add_action( 'login_enqueue_scripts', 'sunflower_login_logo' );
