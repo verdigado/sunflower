b8106db1f5773eee407411bac0f856c8caf5449a
Tom Rose <mail@tom-rose.de>
2021-02-21 23:21:41 +0100

    style comments

--- functions/comments.php
+++ functions/comments.php
@@ -1,12 +1,8 @@
 <?php
 
 function sunflower_rearrange_comment_fields( $fields ) {
+    $fields[] = '</div>';
 
-    $fields['comment'] = sprintf('<div class="col-6">%s', $fields['comment'] );
-
-    $fields['author'] = sprintf('<div class="col-6 comment-form-meta">%s', $fields['author'] );
-    $fields['cookies'] = sprintf('%s</div>', $fields['cookies'] );
-        
     return $fields;
 }
 
