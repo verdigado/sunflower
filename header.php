ddad80081dd390482e55b084688258a5447626d8
Tom Rose <mail@tom-rose.de>
2021-02-24 21:19:49 +0100

    add favicon

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="shortcut icon" href="#">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sunflower.svg">
 
 	<?php wp_head(); ?>
 </head>
