558c8d052aa6da8d4f467fdb6afc06bbf3deb560
Tom Rose <mail@tom-rose.de>
2021-03-20 11:45:12 +0100

    allow custom categories in latest-posts block

--- functions/blocks/latest-posts.php
+++ functions/blocks/latest-posts.php
@@ -1,56 +1,70 @@
 <?php
 
 function sunflower_latest_posts_render( $block_attributes, $content ) {
+    $wp_query_args = array(
+        'post_type'         => 'post',
+        'order'             => 'DESC',
+    );
 
-$posts_per_page = 4; 
-$posts = new WP_Query(array(
-    'post_type'     => 'post',
-    'posts_per_page'=> $posts_per_page,
-    'order'        => 'DESC'
-));
+    if(isset($block_attributes['categories']) AND $block_attributes['categories'] != '' ){
+        $wp_query_args['category_name'] = $block_attributes['categories'];
+    }
 
+    if(isset($block_attributes['count']) AND $block_attributes['count'] != '' ){
+        $wp_query_args['posts_per_page'] = (int) $block_attributes['count'];
+    }else{
+        $wp_query_args['posts_per_page'] = 4;
+    }
 
-$return = sprintf(' <div class="wp-block-group has-background latest-posts">
-                    <div class="wp-block-group__inner-container">
-                        <h2 class="text-center h1">%s</h2>
-                    <div class="row">',
-                    __('News', 'sunflower')
-);
+    if(isset($block_attributes['title']) ){
+        $title = $block_attributes['title'];
+    }else{
+        $title =  __('News', 'sunflower');
+    }
 
+    $posts = new WP_Query($wp_query_args);
 
-ob_start();
-$i = 1;
-echo '<div class="col-12 col-md-6">';
-while ( $posts->have_posts() ) {
-    $posts->the_post();
-        get_template_part( 'template-parts/content', 'archive');
-        if($i == floor( $posts_per_page / 2)){
-            echo '</div><div class="col-12 col-md-6">';
-        }
-        $i++;
-}  
-echo '</div>';
- 
-       
-$return .= ob_get_contents();
-ob_end_clean();
+    $return = sprintf(' <div class="wp-block-group has-background latest-posts">
+                        <div class="wp-block-group__inner-container">
+                            <h2 class="text-center h1">%s</h2>
+                        <div class="row">',
+                        $title
+    );
 
 
-$return .= sprintf('<div class="col-12 col-md-6 offset-md-6">
-        <a class="text-white no-link d-block bg-primary hover-bg-green-550 border-radius" href="%2$s?post_type=post" rel="">
-            <div class="p-45 row ">
-               <span class="h2 col-12 col-md-6">%1$s</span>
-               <span class="continue-reading text-white col-12 col-md-6">%3$s</span>
+    ob_start();
+    $i = 1;
+    echo '<div class="col-12 col-md-6">';
+    while ( $posts->have_posts() ) {
+        $posts->the_post();
+            get_template_part( 'template-parts/content', 'archive');
+            if($i == floor( $posts->post_count / 2)){
+                echo '</div><div class="col-12 col-md-6">';
+            }
+            $i++;
+    }  
+    echo '</div>';
+    
+        
+    $return .= ob_get_contents();
+    ob_end_clean();
+
+
+    $return .= sprintf('<div class="col-12 col-md-6 offset-md-6">
+            <a class="text-white no-link d-block bg-primary hover-bg-green-550 border-radius" href="%2$s?post_type=post" rel="">
+                <div class="p-45 row ">
+                <span class="h2 col-12 col-md-6">%1$s</span>
+                <span class="continue-reading text-white col-12 col-md-6">%3$s</span>
+                </div>
+            </a>
             </div>
-        </a>
-        </div>
-     ',
-    __('more posts', 'sunflower'), 
-    home_url(),
-    __('to archive', 'sunflower')
-);
+        ',
+        __('more posts', 'sunflower'), 
+        home_url(),
+        __('to archive', 'sunflower')
+    );
 
-$return .= '</div></div></div>';
+    $return .= '</div></div></div>';
 
-return $return;
+    return $return;
 }
--- src/latest-posts.js
+++ src/latest-posts.js
@@ -1,36 +1,114 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { withSelect } from '@wordpress/data';
 import { useBlockProps } from '@wordpress/block-editor';
+import {
+	CheckboxControl,
+	RadioControl,
+	TextControl,
+	ToggleControl,
+	SelectControl
+} from '@wordpress/components';
  
 registerBlockType( 'sunflower/latest-posts', {
     apiVersion: 2,
     title: 'Neueste Beiträge',
     icon: 'admin-post',
     category: 'sunflower-blocks',
+    attributes: {
+        categories: {
+			type: 'string',
+			default: ''
+		},
+        count: {
+			type: 'string',
+			default: ''
+		},
+        title: {
+			type: 'string',
+			default: ''
+		},
+    },
  
-    edit: withSelect( ( select ) => {
-        return {
-            posts: select( 'core' ).getEntityRecords( 'postType', 'post' ),
-        };
-    } )( ( { posts } ) => {
+    edit: (props)  => {
         const blockProps = useBlockProps();
- 
+
+        const {
+            attributes: {
+                categories,
+                count,
+                title
+            },
+        } = props;
+
+        const get_wp_categories = ( input ) => {
+            let categories = new Array();
+            wp.data.select('core').getEntityRecords('taxonomy', 'category').forEach(element => {
+                categories.push( element.slug );
+            });
+
+            return categories;
+        };
+    
+        const onChangeCategories = ( input ) => {
+            props.setAttributes( { categories: input === undefined ? '' : input } );
+        };
+
+        const onChangeCount = ( input ) => {
+            props.setAttributes( { count: input === undefined ? '' : input } );
+        };
+
+        const onChangeTitle = ( input ) => {
+            props.setAttributes( { title: input === undefined ? '' : input } );
+        };
+
+
+        const { InspectorControls } = wp.blockEditor;
+        const { PanelBody } = wp.components;
+
+
         return (
             <div { ...blockProps }>
-                { ! posts && 'Loading' }
-                { posts && posts.length === 0 && 'No Posts' }
-                { posts && posts.length > 0 && (
-                    <span> Zeige die neuesten Beiträge an, derzeit  
-                        <ol>
-                            <li>{ posts[ 0 ].title.rendered }</li>
-                            <li>{ posts[ 1 ].title.rendered }</li>
-                            <li>{ posts[ 2 ].title.rendered }</li>
-                            <li>...</li>
-                        </ol>
+                { 
+                    <span> 
+                        Zeige die neuesten Beiträge an. Kategorien und Anzahl kannst Du rechts einstellen. 
+                        Hier klicken für eine Liste aller Kategorien. 
+                        <ul>
+                            {get_wp_categories().map((category, i) => {     
+                                return(<li>{category}</li>);
+                            })}
+                        </ul>
                     </span>
-                ) } 
+                }
+                {
+                    <InspectorControls>
+
+                    <PanelBody title={ 'Einstellungen' }>
+
+                        <TextControl
+                            label="Überschrift"
+                            help="Title der Sektion"
+                            value={ title }
+                            onChange={ onChangeTitle }
+                        />
+
+                        <TextControl
+                            label="Kategorien"
+                            help="Kategorie-Slug(URL) eintragen. Mehrere mit Komma trennen."
+                            value={ categories }
+                            onChange={ onChangeCategories }
+                        />
+
+                        <TextControl
+                            label="Anzahl"
+                            help="Wieviele Beiträge sollen angezeigt werden"
+                            value={ count }
+                            onChange={ onChangeCount }
+                        />
+                    </PanelBody>
+                 
+                 </InspectorControls>
+                }
             </div>
         )
  
-    } ),
-} );
+    },
+} )
