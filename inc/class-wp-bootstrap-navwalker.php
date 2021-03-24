fb12c84e7fbc5ac1b3afd902b4fcb4be5788df19
Tom Rose <mail@tom-rose.de>
2021-03-25 00:00:50 +0100

    make top level menu items clickable

--- inc/class-wp-bootstrap-navwalker.php
+++ inc/class-wp-bootstrap-navwalker.php
@@ -198,8 +198,8 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) {
 			$atts['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
 			// If item has_children add atts to <a>.
 			if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && $args->depth > 1 ) {
-				$atts['href']          = '#';
-				$atts['data-bs-toggle']   = 'dropdown';
+				$atts['href']          = ! empty( $item->url ) ? $item->url : '#';
+				//$atts['data-bs-toggle']   = 'dropdown';
 				$atts['aria-haspopup'] = 'true';
 				$atts['aria-expanded'] = 'false';
 				$atts['class']         = 'dropdown-toggle nav-link';
