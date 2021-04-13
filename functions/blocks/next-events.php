3bc98bbbb1063686cc61eb3830eb611322f36b3f
Tom Rose <mail@tom-rose.de>
2021-04-13 14:26:21 +0200

    add rewrite rules for next events

--- functions/blocks/next-events.php
+++ functions/blocks/next-events.php
@@ -5,16 +5,25 @@ function sunflower_next_events_render( $block_attributes, $content ) {
 
     $classes = (isset($block_attributes['className'])) ? $block_attributes['className'] : '';
 
+    $rules = get_option( 'rewrite_rules' );
+    if($rules){
+        $link = home_url() . '/' . __('events', 'sunflower');
+    }else{
+        $link = home_url() . '?post_type=sunflower_event';
+    }
+
     $return = sprintf(' <div class="wp-block-group has-background next-events %s">
                         <div class="wp-block-group__inner-container">
                             <h2 class="text-center h1">%s</h2>
-                            <div class="wp-block-button text-center mb-5"><a class="wp-block-button__link no-border-radius" href="%s?post_type=sunflower_event" rel="">%s</a></div>
+                            <div class="wp-block-button text-center mb-5"><a class="wp-block-button__link no-border-radius" href="%s" rel="">%s</a></div>
                         <div class="row">',
                         $classes,
                         __('Next events', 'sunflower'),
-                        home_url(),
+                        $link,
                         __('all events', 'sunflower')
     );
+
+    
 
     ob_start();
     switch($next_events->post_count){
