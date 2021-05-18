01bf77d88f7865c01070e6ab84d5fa21a41869bf
Tom Rose <mail@tom-rose.de>
2021-05-18 14:16:10 +0200

    add hint: no upcoming events

--- archive-sunflower_event.php
+++ archive-sunflower_event.php
@@ -66,7 +66,7 @@ get_header();
 
 					else :
 
-						get_template_part( 'template-parts/content', 'none' );
+						get_template_part( 'template-parts/content', 'no-events' );
 
 					endif;
 					?>
---
+++ template-parts/content-no-events.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package sunflower
+ */
+
+?>
+
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'sunflower' ); ?></h1>
+	</header><!-- .page-header -->
+
+	<div class="page-content">
+		<?php
+            _e( 'No upcoming events.', 'sunflower' ); 
+		?>
+	</div><!-- .page-content -->
+</section><!-- .no-results -->
