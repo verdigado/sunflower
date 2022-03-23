ad82be6fc40455eeed5afe40b6d980a9ff2c1f04
Tom Rose <mail@tom-rose.de>
2022-03-23 22:20:55 +0100

    add ics calendar feed

--- functions/ical.php
+++ functions/ical.php
@@ -1,52 +1,26 @@
 <?php
-$_sunflower_event_from = @get_post_meta($post->ID, '_sunflower_event_from')[0] ?: false;
-$_sunflower_event_from = strToTime($_sunflower_event_from);
-$_sunflower_event_until = @get_post_meta($post->ID, '_sunflower_event_until')[0] ?: false;
-$_sunflower_event_until = strToTime($_sunflower_event_until);
-$_sunflower_event_whole_day = @get_post_meta($post->ID, '_sunflower_event_whole_day')[0] ?: false;
-$_sunflower_event_location_name = @get_post_meta($post->ID, '_sunflower_event_location_name')[0] ?: false;
-$_sunflower_event_location_street = @get_post_meta($post->ID, '_sunflower_event_location_street')[0] ?: false;
-$_sunflower_event_location_city = @get_post_meta($post->ID, '_sunflower_event_location_city')[0] ?: false;
 
-
-
-
-
-$from = getIcalDate($_sunflower_event_from, !$_sunflower_event_whole_day);
-$until = ($_sunflower_event_until) ? getIcalDate($_sunflower_event_until, !$_sunflower_event_whole_day) : getIcalDate(3600 + $_sunflower_event_from, !$_sunflower_event_whole_day);
-
-$now = getIcalDate(strToTime('now'), true);
-$summary = get_the_title();
 $proid = parse_url(get_bloginfo('url'), PHP_URL_HOST);
-$uid = md5(uniqid(mt_rand(), true)) . '@' . $proid;
-$description = get_the_excerpt();
-$filename = preg_replace('/[^a-zA-Z0-9]/', '-', $summary) . '.ics';
-$location = join(
-    ', ', array_diff(
-        [
-        $_sunflower_event_location_name, 
-        $_sunflower_event_location_street, 
-        $_sunflower_event_location_city    ], 
-        [false]
-    )
-);
+$filename = preg_replace('/[^a-zA-Z0-9]/', '-', get_the_title()) . '.ics';
 
-$ical=<<<ICAL
+$event = '';
+if (!defined('SUNFLOWER_ICAL_ALL_EVENTS')) {
+    $event = sunflower_getEventInIcs($post) . "\n";
+} else {
+    $filename = preg_replace('/[^a-zA-Z0-9]/', '-', $proid . '_events') . '.ics';
+    $posts = sunflower_get_next_events();
+    while ( $posts->have_posts() ) {
+        $posts->the_post();
+        $event .= sunflower_getEventInIcs($post) . "\n";
+    }
+}
+
+$ical=<<<"ICAL"
 BEGIN:VCALENDAR\r
 VERSION:2.0\r
 PRODID:$proid\r
 METHOD:PUBLISH\r
-BEGIN:VEVENT\r
-UID:$uid\r
-LOCATION:$location\r
-SUMMARY:$summary\r
-DESCRIPTION:$description\r
-CLASS:PUBLIC\r
-DTSTART:$from\r
-DTEND:$until\r
-DTSTAMP:$now\r
-END:VEVENT\r
-END:VCALENDAR\r
+{$event}END:VCALENDAR\r
 ICAL;
 
 header("Pragma: public");
@@ -63,7 +37,52 @@ header("Content-Transfer-Encoding: binary");
 echo $ical;
 die();
 
-function getIcalDate($timestamp, $withTime = false)
+function sunflower_getIcalDate($timestamp, $withTime = false)
 {
     return date('Ymd' . ($withTime ? '\THis' : ''), $timestamp);
+}
+
+function sunflower_getEventInIcs( $post ) 
+{
+    $_sunflower_event_from = @get_post_meta($post->ID, '_sunflower_event_from')[0] ?: false;
+    $_sunflower_event_from = strToTime($_sunflower_event_from);
+    $_sunflower_event_until = @get_post_meta($post->ID, '_sunflower_event_until')[0] ?: false;
+    $_sunflower_event_until = strToTime($_sunflower_event_until);
+    $_sunflower_event_whole_day = @get_post_meta($post->ID, '_sunflower_event_whole_day')[0] ?: false;
+    $_sunflower_event_location_name = @get_post_meta($post->ID, '_sunflower_event_location_name')[0] ?: false;
+    $_sunflower_event_location_street = @get_post_meta($post->ID, '_sunflower_event_location_street')[0] ?: false;
+    $_sunflower_event_location_city = @get_post_meta($post->ID, '_sunflower_event_location_city')[0] ?: false;
+
+    $from = sunflower_getIcalDate($_sunflower_event_from, !$_sunflower_event_whole_day);
+    $until = ($_sunflower_event_until) ? sunflower_getIcalDate($_sunflower_event_until, !$_sunflower_event_whole_day) : sunflower_getIcalDate(3600 + $_sunflower_event_from, !$_sunflower_event_whole_day);
+
+    $now = sunflower_getIcalDate(strToTime('now'), true);
+    $summary = get_the_title();
+    $proid = parse_url(get_bloginfo('url'), PHP_URL_HOST);
+    $uid = md5(uniqid(mt_rand(), true)) . '@' . $proid;
+    $description = get_the_excerpt();
+    $location = join(
+        ', ', array_diff(
+            [
+            $_sunflower_event_location_name, 
+            $_sunflower_event_location_street, 
+            $_sunflower_event_location_city    ], 
+            [false]
+        )
+    );
+
+    $event=<<<"ICALEVENT"
+BEGIN:VEVENT\r
+UID:$uid\r
+LOCATION:$location\r
+SUMMARY:$summary\r
+DESCRIPTION:$description\r
+CLASS:PUBLIC\r
+DTSTART:$from\r
+DTEND:$until\r
+DTSTAMP:$now\r
+END:VEVENT\r
+ICALEVENT;
+
+    return $event;
 }
