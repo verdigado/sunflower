6ab304eccc5bd12d96b873fc2cf7110820751abb
Alexander Bigga <alexander.bigga@verdigado.com>
2023-11-16 11:48:50 +0100

    Update npm packages

--- functions/_s.php
+++ functions/_s.php
@@ -78,7 +78,6 @@ if ( ! function_exists( 'sunflower_setup' ) ) :
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
-        add_theme_support( 'wp-block-styles' );
         add_theme_support( 'align-wide' );
         add_theme_support( 'editor-styles' );
         add_editor_style ( '/assets/css/editor-style.css' );
