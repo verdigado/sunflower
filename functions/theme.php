e4bdb47a4c010286c9e18cdb4cfa821b4d7d6b85
Alexander Bigga <alexander.bigga@verdigado.com>
2024-01-29 14:10:39 +0100

    Add favicon.ico file and replace default redirect to WP favicon

--- functions/theme.php
+++ functions/theme.php
@@ -5,3 +5,12 @@ add_theme_support( 'custom-logo' );
 function the_sunflower_theme() {
 	echo defined( 'SUNFLOWER_THEME' ) ? 'theme--' . SUNFLOWER_THEME : 'theme--default';
 }
+
+
+function sunflower_get_site_icon_url_defaults($url, $size, $blog_id) {
+
+    // Update the $url for the /favicon.ico file
+    return sunflower_parent_or_child( 'assets/img/favicon.ico' );
+}
+// add the filter
+add_filter( "get_site_icon_url", "sunflower_get_site_icon_url_defaults", 10, 3 );
