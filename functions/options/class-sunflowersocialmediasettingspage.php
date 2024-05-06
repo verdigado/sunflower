4190ed377d8248d415707871907099ca47ce8c0a
Alexander Bigga <alexander.bigga@verdigado.com>
2024-05-06 20:01:21 +0200

    Add bluesky and threads to sunflower sharing block (#570)
    * Add bluesky and threads to sunflower sharing block
    
    * add social media tiktok
    
    ---------
    
    Co-authored-by: Hanna Huewe <hanna.huewe@verdigado.com>

--- functions/options/class-sunflowersocialmediasettingspage.php
+++ functions/options/class-sunflowersocialmediasettingspage.php
@@ -191,7 +191,10 @@ class SunflowerSocialMediaSettingsPage {
 		$default   = array();
 		$default[] = 'fab fa-x-twitter;X (Twitter);';
 		$default[] = 'fab fa-twitter;Twitter;';
-		$default[] = 'fab fa-facebook;Facebook;';
+		$default[] = 'fab fa-bluesky;Bluesky;';
+		$default[] = 'fab fa-facebook-f;Facebook;';
+		$default[] = 'fab fa-threads;Threads;';
+		$default[] = 'fab fa-tiktok;TikTok;';
 		$default[] = 'fab fa-linkedin;LinkedIn;';
 		$default[] = 'fab fa-instagram;Instagram;';
 		$default[] = 'fab fa-youtube;YouTube;';
