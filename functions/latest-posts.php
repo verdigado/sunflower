135461de1e004fc130b0162f3a6cd3ccd2d8bf9c
Alexander Bigga <alexander.bigga@verdigado.com>
2024-08-08 20:23:03 +0200

    Fix excluded categories in latest-posts

--- functions/latest-posts.php
+++ functions/latest-posts.php
@@ -15,41 +15,43 @@
  * @return WP_Query
  */
 function sunflower_get_latest_posts( $number = -1, $category_ids = null, $exclude_ids = null ) {
-	$tax_query = null;
+	$tax_query = array();
 
-	if ( $category_ids || $exclude_ids ) {
+	if ( $exclude_ids ) {
+		array_push(
+			$tax_query,
+			array(
+				'taxonomy' => 'category',
+				'field'    => 'slug',
+				'operator' => 'NOT IN',
+				'terms'    => $exclude_ids,
+			)
+		);
+	}
+
+	if ( $category_ids ) {
 		// In sunflower < 2.1.0, category_ids is a comma separated string.
 		if ( ! is_array( $category_ids ) ) {
 			$category_ids = explode( ',', $category_ids );
 		}
 
 		if ( sunflower_is_numeric_array( $category_ids ) ) {
-			$tax_query = array(
+			array_push(
+				$tax_query,
 				array(
 					'taxonomy' => 'category',
 					'field'    => 'id',
 					'terms'    => $category_ids,
-				),
-				array(
-					'taxonomy' => 'category',
-					'field'    => 'slug',
-					'operator' => 'NOT IN',
-					'terms'    => $exclude_ids,
-				),
+				)
 			);
 		} else {
-			$tax_query = array(
+			array_push(
+				$tax_query,
 				array(
 					'taxonomy' => 'category',
 					'field'    => 'slug',
 					'terms'    => $category_ids,
-				),
-				array(
-					'taxonomy' => 'category',
-					'field'    => 'slug',
-					'operator' => 'NOT IN',
-					'terms'    => $exclude_ids,
-				),
+				)
 			);
 		}
 	}
