7906679b0fdf0ff63e9f4b2b8b0e04ec03aa1c6e
Alexander Bigga <alexander.bigga@verdigado.com>
2024-11-11 13:10:14 +0100

    Avoid warnings on import old wpcalendar events

--- functions/activation.php
+++ functions/activation.php
@@ -56,7 +56,7 @@ function sunflower_import_events() {
 		$post->ID        = 0;
 		$post->post_type = 'sunflower_event';
 
-		if ( $meta['_secretevent'][0] ) {
+		if ( isset( $meta['_secretevent'][0] ) && true === $meta['_secretevent'][0] ) {
 			$post->post_status = 'draft';
 		}
 
@@ -68,18 +68,18 @@ function sunflower_import_events() {
 
 		update_post_meta( $sunflower_original_post_id, '_sunflower_copied', true );
 
-		update_post_meta( $id, '_sunflower_event_location_city', $meta['_geostadt'][0] );
-		update_post_meta( $id, '_sunflower_event_location_name', $meta['_geoshow'][0] );
-		update_post_meta( $id, '_sunflower_event_lat', $meta['_lat'][0] );
-		update_post_meta( $id, '_sunflower_event_lon', $meta['_lon'][0] );
-		update_post_meta( $id, '_sunflower_event_zoom', $meta['_zoom'][0] );
-		update_post_meta( $id, '_sunflower_event_organizer', $meta['_veranstalter'][0] );
-		update_post_meta( $id, '_sunflower_event_organizer_url', $meta['_veranstalterlnk'][0] );
+		update_post_meta( $id, '_sunflower_event_location_city', $meta['_geostadt'][0] ?? '' );
+		update_post_meta( $id, '_sunflower_event_location_name', $meta['_geoshow'][0] ?? '' );
+		update_post_meta( $id, '_sunflower_event_lat', $meta['_lat'][0] ?? '' );
+		update_post_meta( $id, '_sunflower_event_lon', $meta['_lon'][0] ?? '' );
+		update_post_meta( $id, '_sunflower_event_zoom', $meta['_zoom'][0] ?? '' );
+		update_post_meta( $id, '_sunflower_event_organizer', $meta['_veranstalter'][0] ?? '' );
+		update_post_meta( $id, '_sunflower_event_organizer_url', $meta['_veranstalterlnk'][0] ?? '' );
 
-		update_post_meta( $id, '_sunflower_event_from', sunflower_german_date2int_date( $meta['_wpcal_from'][0] ) );
-		update_post_meta( $id, '_sunflower_event_until', sunflower_german_date2int_date( $meta['_bis'][0] ) );
+		update_post_meta( $id, '_sunflower_event_from', sunflower_german_date2int_date( $meta['_wpcal_from'][0] ?? '' ) );
+		update_post_meta( $id, '_sunflower_event_until', sunflower_german_date2int_date( $meta['_bis'][0] ?? '' ) );
 
-		if ( $meta['_thumbnail_id'][0] ) {
+		if ( isset( $meta['_thumbnail_id'][0] ) ) {
 			update_post_meta( $id, '_thumbnail_id', $meta['_thumbnail_id'][0] );
 		}
 	}
