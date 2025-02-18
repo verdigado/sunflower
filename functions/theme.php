1c8f8ce62a39515068a7884aa9fd449247921152
Alexander Bigga <alexander.bigga@verdigado.com>
2025-02-18 14:15:44 +0100

    Provide all resolutions of website icons; remove duplicated icon metadata (#810)

--- functions/theme.php
+++ functions/theme.php
@@ -21,17 +21,39 @@ function sunflower_theme_class() {
  * Change URL to site icon to sunflower as default.
  *
  * @param string $url     Site icon URL.
+ * @param string $size    Size in pixel.
  */
-function sunflower_get_site_icon_url_defaults( $url ) {
+function sunflower_get_site_icon_url_defaults( $url, $size ) {
+
+	$site_icon_id = (int) get_option( 'site_icon' );
+
 	// A custom site icon seems to be set. Keep it untouched.
-	if ( filter_var( $url, FILTER_VALIDATE_URL ) ) {
+	if ( $site_icon_id && filter_var( $url, FILTER_VALIDATE_URL ) ) {
 		return $url;
 	}
+
+	switch ( $size ) {
+		case 32:
+			$icon_url = sunflower_parent_or_child( 'assets/img/favicon.ico' );
+			break;
+		case 192:
+			$icon_url = sunflower_parent_or_child( 'assets/img/sunflower-192.png' );
+			break;
+		case 180:
+			$icon_url = sunflower_parent_or_child( 'assets/img/sunflower-180.png' );
+			break;
+		case 270:
+			$icon_url = sunflower_parent_or_child( 'assets/img/sunflower-270.png' );
+			break;
+		default:
+			$icon_url = sunflower_parent_or_child( 'assets/img/sunflower.png' );
+			break;
+	}
 	// Set default site icon to sunflower.
-	return sunflower_parent_or_child( 'assets/img/favicon.ico' );
+	return $icon_url;
 }
 
-// Add filter only, of terms of use are accepted.
+// Add filter only, if terms of use are accepted.
 $sunflower_options = get_option( 'sunflower_first_steps_options' );
 if ( ( $sunflower_options['sunflower_terms_of_use'] ?? false ) === 'checked' ) {
 	add_filter( 'get_site_icon_url', 'sunflower_get_site_icon_url_defaults', 10, 3 );
--- header.php
+++ header.php
@@ -18,13 +18,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php
-		$sunflower_options = get_option( 'sunflower_first_steps_options' );
-	if ( ( $sunflower_options['sunflower_terms_of_use'] ?? false ) === 'checked' ) {
-		printf( '<link rel="icon" href="%s" type="image/svg+xml" sizes="any">', esc_attr( sunflower_parent_or_child( 'assets/img/sunflower.svg' ) ) );
-		printf( '<link rel="icon" href="%s" type="image/png" sizes="48x48">', esc_attr( sunflower_parent_or_child( 'assets/img/favicon-48x48.png' ) ) );
-	}
-	?>
-	<?php
 	get_template_part( 'template-parts/open-graph' );
 	?>
 	<?php wp_head(); ?>
