be0c95c6abe0a20810e45990087d2f30cbbb03fe
Alexander Bigga <alexander.bigga@verdigado.com>
2025-04-01 21:36:42 +0200

    Fix styles of buttons in backend

--- inc/class-wp-bootstrap-navwalker.php
+++ inc/class-wp-bootstrap-navwalker.php
@@ -245,7 +245,7 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) :
 
 			// If the item has_children add atts to <a>.
 			if ( $this->has_children && 0 === $depth ) {
-				$atts['href']          = '#';
+				$atts['href']          = empty( $item->url ) ? '#' : $item->url;
 				$atts['data-toggle']   = 'dropdown';
 				$atts['aria-expanded'] = 'false';
 				$atts['class']         = 'dropdown-toggle nav-link';
