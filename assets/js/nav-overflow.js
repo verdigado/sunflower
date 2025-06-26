21ddd7c455375bc832b860b8883692fc4bdedd7c
DDEV User <nobody@example.com>
2025-06-26 11:04:28 +0200

    Revert "debug nav-overflow server"
    This reverts commit 39ba1dfd5666f850aee39c2e858d8395335fa8ed.

--- assets/js/nav-overflow.js
+++ assets/js/nav-overflow.js
@@ -2,10 +2,6 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	const navWrapper = document.querySelector( '.right-bar' );
 	const menuUl = document.querySelector( '#menu-neues-hauptmenue' );
 
-	console.log('--- DEBUG nav-overflow.js ---');
-    console.log('navWrapper:', navWrapper);
-    console.log('menuUl:', menuUl);
-
 	if ( ! navWrapper || ! menuUl ) {
 		return;
 	}
