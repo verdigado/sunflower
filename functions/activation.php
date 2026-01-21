dfeac1f36b89972b2e91f5b876fd6073c556d86c
Alexander Bigga <alexander.bigga@verdigado.com>
2026-01-21 09:01:24 +0100

    Fix activation of theme

--- functions/activation.php
+++ functions/activation.php
@@ -9,8 +9,7 @@
  * Jobs run after activation of sunflower theme.
  */
 function sunflower_activate_theme() {
-	sunflower_import_widgets();
-	sunflower_import_events();
+	// Placeholder for future activation tasks.
 }
 
 add_action( 'after_switch_theme', 'sunflower_activate_theme', 10, 2 );
