8756760a0795162bb1173aa3b6e7257d77cc48b8
Alexander Bigga <alexander.bigga@verdigado.com>
2026-08-26 14:08:23 +0200

    Harden the contact form, captcha optional (#1428)
    * Fix language files again
    * Harden the contact form, captcha optional

--- src/contact-form/render.php
+++ src/contact-form/render.php
@@ -11,6 +11,7 @@ $sunflower_sendcopy      = $attributes['sendCopy'] ?? 0;
 $sunflower_display_phone = $attributes['displayPhone'] ?? false;
 $sunflower_require_phone = $attributes['requirePhone'] ?? false;
 $sunflower_require_mail  = $attributes['requireMail'] ?? false;
+$sunflower_show_captcha  = $attributes['showCaptcha'] ?? true;
 
 // Placeholder with '*' appended if required.
 $sunflower_placeholder_message = __( 'Your Message', 'sunflower-contact-form' ) . '*';
@@ -38,6 +39,22 @@ $sunflower_placeholder_captcha = sprintf( __( 'How much is %s?', 'sunflower-cont
 
 	<form id="sunflower-contact-form" method="post" class="row">
 		<?php wp_nonce_field( 'sunflower_contact_form' ); ?>
+		<?php
+		// Spam protection: signed render timestamp (time-trap) + captcha-enabled flag.
+		// The flag is inside the HMAC so a bot cannot switch the captcha off by tampering.
+		$sunflower_form_ts      = time();
+		$sunflower_captcha_flag = $sunflower_show_captcha ? '1' : '0';
+		$sunflower_form_ts_sig  = hash_hmac( 'sha256', $sunflower_form_ts . '|' . $sunflower_captcha_flag, $sunflower_captcha_salt );
+		?>
+		<input type="hidden" name="form_ts" value="<?php echo esc_attr( $sunflower_form_ts ); ?>" />
+		<input type="hidden" name="form_ts_sig" value="<?php echo esc_attr( $sunflower_form_ts_sig ); ?>" />
+		<input type="hidden" name="captcha_on" value="<?php echo esc_attr( $sunflower_captcha_flag ); ?>" />
+
+		<?php // Honeypot: hidden from humans; bots that fill it are rejected. ?>
+		<div class="comment-form-website" aria-hidden="true" style="position:absolute;left:-5000px;top:auto;width:1px;height:1px;overflow:hidden;">
+			<label for="website"><?php esc_html_e( 'Leave this field empty', 'sunflower-contact-form' ); ?></label>
+			<input id="website" name="website" type="text" tabindex="-1" autocomplete="off" value="" />
+		</div>
 
 		<div class="col-12 col-md-6">
 			<div class="comment-form-comment">
@@ -113,6 +130,7 @@ $sunflower_placeholder_captcha = sprintf( __( 'How much is %s?', 'sunflower-cont
 			</div>
 			<?php endif; ?>
 
+			<?php if ( $sunflower_show_captcha ) : ?>
 			<div class="comment-form-email">
 				<label for="captcha">
 				<?php
@@ -137,6 +155,7 @@ $sunflower_placeholder_captcha = sprintf( __( 'How much is %s?', 'sunflower-cont
 				</div>
 
 			</div>
+			<?php endif; ?>
 
 			<div>
 				<p class="small">
